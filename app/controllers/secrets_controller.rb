41f6351e81d2d8a375a745067bfe1052e9053507
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-18 23:38:49 +0000

    Automatically backed up by Learn

--- app/controllers/secrets_controller.rb
+++ app/controllers/secrets_controller.rb
@@ -1,18 +1,7 @@
 class SecretsController < ApplicationController
-
+  before_action :require_logged_in
 
   def show
   end
-  
-  def create
-    return redirect_to(controller: 'sessions',
-                           action: 'new') if !params[:name] || params[:name].empty?
-        session[:name] = params[:name]
-        redirect_to controller: 'application', action: 'hello'
-      end
 
-      def destroy
-        session.delete :name
-        redirect_to controller: 'application', action: 'hello'
-      end
 end
