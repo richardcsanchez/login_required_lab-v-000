d19b3535901f8fef10dce6e2c26f2c455966ebe6
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-18 23:43:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,9 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def hello
+    redirect_to controller: 'sessions' action: 'new' unless current_user
+  end
   def current_user
     session[:name]
   end
@@ -10,7 +13,7 @@ class ApplicationController < ActionController::Base
   private
 
   def require_logged_in
-    redirect_to controller :sessions, action: 'new' unless current_user
+    redirect_to controller: 'sessions', action: 'new' unless current_user
   end
 
 end
