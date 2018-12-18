c2ea79e171ef4bfe139f13e43d0dd7252c97fcab
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-18 23:16:49 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  resources 'sessions'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
