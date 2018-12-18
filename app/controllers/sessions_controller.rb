6090a7e117c42beb46c2f281c9fdbad486886574
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-18 23:38:40 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,18 @@
 class SessionsController < ApplicationController
-  before_action :require_logged_in
 
   def show
   end
+
+  def create
+    return redirect_to(controller: 'sessions',
+                           action: 'new') if !params[:name] || params[:name].empty?
+        session[:name] = params[:name]
+        redirect_to controller: 'application', action: 'hello'
+      end
+
+  def destroy
+    session.delete :name
+    redirect_to controller: 'application', action: 'hello'
+  end
+
 end
