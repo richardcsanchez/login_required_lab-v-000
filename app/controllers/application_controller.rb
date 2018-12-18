4ba1625a2867634eab6ef2f1e14c6deef1a033ae
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-18 23:41:38 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,10 @@ class ApplicationController < ActionController::Base
   def current_user
     session[:name]
   end
+
+  private
+
+  def require_logged_in
+    redirect_to controller :sessions, action: = 'new' unless current_user
+  end
 end
