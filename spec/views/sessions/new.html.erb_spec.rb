bb8af9c31b7887fd506a0eff34efc2e2a1b74855
Richard Sanchez <richard.charles.s@gmail.com>
2018-12-19 00:02:08 +0000

    Automatically backed up by Learn

--- spec/views/sessions/new.html.erb_spec.rb
+++ spec/views/sessions/new.html.erb_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe "sessions/new.html.erb", type: :view do
 
-  it "shows login link if not logged in" do
-    session[:name] = nil
-    render :template => 'application/hello.html.erb'
+  it "shows login button" do
+    render 
     expect(rendered).to include '/login'
   end
-  
+
 end
